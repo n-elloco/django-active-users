759776b7b092d71aaa4741613ee4f3054313241d
Nikita Ekaterinchuk <ekaterinchuk@bars-open.ru>
2016-04-12 11:56:13 +0600

    Version 0.0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-active-users',
-    version='0.0.1.2',
+    version='0.0.1.3',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     url='',
