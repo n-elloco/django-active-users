36d07de8616fe13a9eaebe36ce3201d1af202bd5
Nikita Ekaterinchuk <en.elloco@gmail.com>
2016-10-03 11:15:54 +0700

    Version 0.1

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 setup(
     name='django-active-users',
-    version='0.0.1.4',
+    version='0.1',
     packages=['active_users'],
     url='https://github.com/n-elloco/django-active-users',
     license='MIT',
     author='Nikita Ekaterinchuk',
-    author_email='',
+    author_email='en.elloco@gmail.com',
     description='Monitoring of active users in Django using Redis',
     long_description=open('README.rst').read(),
     install_requires=open('REQUIREMENTS').read(),
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
