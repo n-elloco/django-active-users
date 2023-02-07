cc578ae50e05ac4000653bd2a6954e060d08796b
Nikita Ekaterinchuk <n-elloco@users.noreply.github.com>
2023-02-07 18:33:29 +0200

    Fixed build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-active-users',
-    version='0.3.0',
+    version='0.4.0',
     packages=['active_users', 'active_users.api'],
     url='https://github.com/n-elloco/django-active-users',
     license='MIT',
